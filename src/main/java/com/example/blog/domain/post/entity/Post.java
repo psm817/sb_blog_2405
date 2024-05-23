721d55d59fd2b610bea8998aad979ea30c79eed8
psm817 <5004jse@gmail.com>
2024-05-23 16:44:01 +0900

    게시글 리스트 데이터 출력

--- src/main/java/com/example/blog/domain/post/entity/Post.java
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -3,12 +3,18 @@ package com.example.blog.domain.post.entity;
 import com.example.blog.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Post extends BaseEntity {
     private String title;
     private String content;
