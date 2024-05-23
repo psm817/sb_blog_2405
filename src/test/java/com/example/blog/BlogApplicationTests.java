c0743d8ec9c652f170055a5c981a381b114f1010
psm817 <5004jse@gmail.com>
2024-05-23 16:21:15 +0900

    빌더 패턴 service 적용, 테스트 데이터 생성

--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.blog;
 
+import com.example.blog.domain.post.service.PostService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Autowired
+	private PostService postService;
 	@Test
 	void contextLoads() {
+		for(int i = 0; i <= 30; i++) {
+			String title = String.format("제목 %d", i);
+			String content = String.format("내용 %d", i);
+			this.postService.create(title, content);
+		}
 	}
 
 }
