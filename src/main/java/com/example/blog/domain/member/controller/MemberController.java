2541c8bdd8dcc847599447434c57c01ebf04cfb3
psm817 <5004jse@gmail.com>
2024-05-28 14:16:58 +0900

    일반 회원가입 구현

--- src/main/java/com/example/blog/domain/member/controller/MemberController.java
+++ src/main/java/com/example/blog/domain/member/controller/MemberController.java
@@ -1,15 +1,27 @@
 package com.example.blog.domain.member.controller;
 
+import com.example.blog.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
     @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
     public String loginPage() {
@@ -19,5 +31,38 @@ public class MemberController {
     @GetMapping("/signup")
     public String signupPage() {
         return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid SignForm signForm) {
+        memberService.signup(signForm.getUsername(), signForm.getPassword(), signForm.getNickname(), signForm.getEmail());
+
+        return "redirect:/member/login";
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class SignForm {
+        @NotBlank
+        @Length(min = 3)
+        private String username;
+
+        @NotBlank
+        @Length(min = 4)
+        private String password;
+
+        @NotBlank
+        @Length(min = 4)
+        private String password_confirm;
+
+        @NotBlank
+        @Length(min = 3)
+        private String nickname;
+
+        @NotBlank
+        @Length(min = 4)
+        @Email
+        private String email;
     }
 }
